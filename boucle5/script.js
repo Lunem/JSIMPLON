4b174f735d24f9ab8d3c7efd10f6b8a714770756
Lunem <lydia.detienne@gmail.com>
2016-03-06 23:16:42 +0100

    ajout factorielle

--- boucle5/script.js
+++ boucle5/script.js
@@ -3,15 +3,36 @@
 // Écrire un algorithme qui retourne la factorielle de n (entier positif ou nul).
 // Rappel : la factorielle de n, notée n!, est le produit des n premiers entiers strictement positifs. Par convention, 0 ! = 1.
 //- les n premiers entiers strictement positifs ;
+//
+// var nombre = (prompt("Entrez un nombre"));
+//  factorielle = function(nombre){
+//    var compteur = 1;
+//    if (nombre >= 0 && !isNaN(nombre)){
+//      while(compteur <= nombre){
+//        fac = fac * compteurprec;
+//        fac = fac + fac;
+//        compteurprec--;
+//        fac++;
+//        compteur++;
+//      }
+//      resultat = compteur;
+//      console.log(resultat);
+//    }
+// }
+// factorielle(nombre);
+
 
 var nombre = (prompt("Entrez un nombre"));
- factorielle = function(nombre){
+ premierpositif = function(nombre){
    var compteur = 1;
+   var compteur2 = 0;
    if (nombre > 0 && !isNaN(nombre)){
-     while(compteur <= nombre){
-       console.log(compteur);
+     while(compteur < nombre){
        compteur++;
+       compteur2 = compteur;
+       compteur2 = compteur2 * compteur;
      }
+     console.log(compteur2);
    }
 }
-factorielle(nombre);
+premierpositif(nombre);
